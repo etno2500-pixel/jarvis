765c57c83717c3abaa2e3d70c648956fb4ee77e0
etno2500-pixel <etno2500@gmail.com>
2026-08-02 13:58:54 +0200

    Add Kotlin Compose plugin to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ pluginManagement {
         id("org.jetbrains.kotlin.android") version "2.0.21"
         id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
         id("com.google.devtools.ksp") version "2.0.21-1.0.28"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
     }
 }
 
