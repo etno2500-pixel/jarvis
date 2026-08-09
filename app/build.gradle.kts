af2d3b5d001bc20351de5bcf377c3564b448e001
etno2500-pixel <etno2500@gmail.com>
2026-08-09 20:47:16 +0200

    Bump version for UI update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.etno2500pixel.jarvis"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     buildFeatures { compose = true }
